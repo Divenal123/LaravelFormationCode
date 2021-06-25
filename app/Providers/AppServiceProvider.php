e51d287b9684a8e4c3c3373092446d916483cce2
Divenal <divenal.basilua@gmail.com>
2021-06-25 10:06:47 +0100

    Implémentation relation plusieurs à plusieurs

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use App\Models\Category;
+
+class AppServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        View::composer(['index', 'create', 'edit'], function ($view) {
+            $view->with('categories', Category::all());
+        });
+    }
+}
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\{ Film, Category };
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Category::factory()->count(10)->create();
+
+        $ids = range(1, 10);
+
+        Film::factory()->count(40)->create()->each(function ($film) use($ids) {
+            shuffle($ids);
+            $film->categories()->attach(array_slice($ids, 0, rand(1, 4)));
+        });
+    }
+}
